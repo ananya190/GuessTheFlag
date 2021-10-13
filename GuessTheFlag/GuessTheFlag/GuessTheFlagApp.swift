15b31de9ffe03ffee9adb17d5b1185e0aee880e9
Ananya Elizabeth George <50709725+ananya190@users.noreply.github.com>
2021-10-13 10:40:28 +0530

    complete: project 2

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Ananya George on 10/13/21.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
